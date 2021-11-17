fc967477959004786038d3ced29dac5f3b863f14
Ladun Omideyi <Ladun.Omideyi@softwire.com>
2021-11-17 17:56:17 +0000

    Add method for reverse

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Fizzbuzz
 {
@@ -10,6 +12,7 @@ namespace Fizzbuzz
             {
                 return "Fizz";
             }
+
             return "";
         }
 
@@ -19,28 +22,31 @@ namespace Fizzbuzz
             {
                 return "Buzz";
             }
+
             return "";
         }
-        
-        static string Bang (int num)
+
+        static string Bang(int num)
         {
             if (num % 7 == 0)
             {
                 return "Bang";
             }
+
             return "";
         }
-        
-        static string Bong (int num, string myString)
+
+        static string Bong(int num, string myString)
         {
             if (num % 11 == 0)
             {
                 return "Bong";
             }
+
             return myString;
         }
-        
-        static string Fezz (int num, string myString)
+
+        static string Fezz(int num, string myString)
         {
             if (num % 13 == 0 && myString == "")
             {
@@ -50,33 +56,86 @@ namespace Fizzbuzz
 
             if (num % 13 == 0 && myString != "")
             {
-                int index = myString.IndexOf('B');
-
-                if (index > -1)
-                {
-                    string insertedString = myString.Insert(index, "Fezz");
-                    return insertedString;
-                }
-
-                string addedString = myString += "Fezz";
-                return addedString;
+                var insertedString = InsertFezz(myString);
+                return insertedString;
             }
+
             return myString;
         }
-        
-        static string Reverse (int num, string myString)
+
+        static string Reverse(int num, string myString)
         {
             if (num % 17 == 0)
             {
-                
-                return "Bong";
+                if (myString.Contains("Fezz") && myString.Contains("Bong"))
+                {
+                    return myString;
+                }
+
+                if (myString.Contains("Fezz"))
+                {
+                    var fezzlessString = myString.Remove(myString.IndexOf("Fezz"));
+                    var reversedFezzlessString = ReverseWordsInString(fezzlessString);
+                    var reversedFezzfulString = InsertFezz(reversedFezzlessString);
+                    return reversedFezzfulString;
+                }
+
+                var reversedString = ReverseWordsInString(myString);
+                return reversedString;
             }
             return myString;
         }
 
-        static void Main(string[] args)
+        static IEnumerable<string> Split(string str)
         {
-            for (var i = 1; i < 175; i++)
+            while (str.Length > 0)
+            {
+                yield return new string(str.Take(4).ToArray());
+                str = new string(str.Skip(4).ToArray());
+            }
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static string ReverseWordsInString(string myString)
+        {
+            var stringList = Split(myString);
+            List<string> revStringList = new List<string>();
+            foreach (var str in stringList)
+            {
+                var revStr = ReverseString(str);
+                revStringList.Add(revStr);
+            }
+
+            string combinedRevStrs = string.Join("", revStringList);
+            string combinedStrings = ReverseString(combinedRevStrs);
+
+            return combinedStrings;
+        }
+
+        public static string InsertFezz(string myString)
+        {
+            int index = myString.IndexOf('B');
+
+            if (index > -1)
+            {
+                string insertedString = myString.Insert(index, "Fezz");
+                return insertedString;
+            }
+
+            string addedString = myString += "Fezz";
+            return addedString;
+        }
+
+
+    static void Main(string[] args)
+        {
+            for (var i = 1; i < 270; i++)
             {
                 var myString = "";
                 myString += Fizz(i);
