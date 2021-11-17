daf5fc709c70adb769fd600e8dd629e28ed6873d
Ladun Omideyi <Ladun.Omideyi@softwire.com>
2021-11-17 12:27:17 +0000

    Add method to return fizzbuzz

--- Program.cs
+++ Program.cs
@@ -4,9 +4,33 @@ namespace Fizzbuzz
 {
     class Program
     {
+        static string Fizz(int num)
+        {
+            if (num % 3 == 0)
+            {
+                return "Fizz";
+            }
+            return "";
+        }
+
+        static string Buzz(int num2)
+        {
+            if (num2 % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return "";
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (var i = 1; i < 101; i++)
+            {
+                var myString = "";
+                myString += Fizz(i);
+                myString += Buzz(i);
+                Console.WriteLine(myString == "" ? i : myString);
+            }
         }
     }
 }
